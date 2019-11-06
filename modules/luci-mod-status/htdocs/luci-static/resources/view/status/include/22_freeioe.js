d073d1f457bfc2d73855ea59a1a38e5ce4c2af8f
Dirk Chang <dirk@kooiot.com>
2019-11-06 12:08:09 +0800

    show freeioe in standalone zone

---
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/22_freeioe.js
@@ -0,0 +1,48 @@
+'use strict';
+'require fs';
+'require rpc';
+
+var callSystemBoard = rpc.declare({
+	object: 'freeioe',
+	method: 'info'
+});
+
+var callSystemInfo = rpc.declare({
+	object: 'freeioe',
+	method: 'cloud'
+});
+
+return L.Class.extend({
+	title: _('System'),
+
+	load: function() {
+		return Promise.all([
+			L.resolveDefault(callFreeioeInfo(), {}),
+			L.resolveDefault(callFreeioeCloud(), {}),
+		]);
+	},
+
+	render: function(data) {
+		var info   = data[0],
+		    cloud  = data[1];
+
+		var fields = [
+			_('SN'),               info.hw_id + ' / ' + info.id,
+			_('Version'),          info.version,
+			_('Skynet Version'),   info.skynet_version,
+			_('Firmware Version'), info.firmware_version,
+			_('Beta'),             info.beta,
+		];
+
+		var table = E('div', { 'class': 'table' });
+
+		for (var i = 0; i < fields.length; i += 2) {
+			table.appendChild(E('div', { 'class': 'tr' }, [
+				E('div', { 'class': 'td left', 'width': '33%' }, [ fields[i] ]),
+				E('div', { 'class': 'td left' }, [ (fields[i + 1] != null) ? fields[i + 1] : '?' ])
+			]));
+		}
+
+		return table;
+	}
+});
